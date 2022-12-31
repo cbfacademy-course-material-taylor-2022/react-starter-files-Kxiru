1f9769c08a7a88c16c38e2991a9419ca1651a8e5
kxiru <nkeirukaw@gmail.com>
2022-12-31 00:23:27 +0000

    Testing react app task completed

--- first-react-app/src/App.test.js
+++ first-react-app/src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1 = screen.getByText(/My React Page/i);
+  expect(h1).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const h2 = screen.getByText(/This is my first React page.+/i);
+  expect(h2).toBeInTheDocument();
+});
+
+test('renders without crashing', () => {
+  act(() => {
+    const root = ReactDOM.createRoot(document.createElement('div'));
+    root.render(<App />);
+  });
 });
