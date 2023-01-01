d4d3d26114f374326da73813a9c3a4a8fd9a1a01
kxiru <nkeirukaw@gmail.com>
2023-01-01 13:42:39 +0000

    Working with JSX Components task completed

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+
+const name = 'Nkeirux';
 
 function Profile(name){
-    return <main id="content" role="main" className="base">
-            <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
-            </main>
+  let bio = ''
+  let correctName = false;
+
+  if (name.toLowerCase() === 'nkeiru'){
+    bio = 'Young Software Engineer!'
+    correctName = true;
+  }
+
+  return <main id="content" role="main" className="base">
+    <h1>{correctName ? name : "No One"}'s React Page</h1>
+      <p>{bio}</p>
+    </main>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
-
+root.render(Profile(name));
