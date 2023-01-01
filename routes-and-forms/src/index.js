73a0da382e83238c735cc1dc9e95c6dd370c1f8a
kxiru <nkeirukaw@gmail.com>
2023-01-01 21:21:47 +0000

    Adding routes task completed

--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -1,39 +1,73 @@
-import {React} from "react";
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import { React } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
       <ul className="breadcrumb">
-        <li><Link to="/"> All Pets </Link></li>
-        <li><Link to="/kittens"> Kittens </Link></li>
+        <li>
+          <Link to="/"> All Pets </Link>
+        </li>
+        <li>
+          <Link to="/kittens"> Kittens </Link>
+        </li>
+        <li>
+          <Link to="/puppies"> Puppies </Link>
+        </li>
+        <li>
+          <Link to="/ducklings"> Ducklings </Link>
+        </li>
       </ul>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="kittens" element={<Kittens />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/kittens" element={<Kittens />} />
+        <Route path="/puppies" element={<Puppies />} />
+        <Route path="/ducklings" element={<Ducklings />} />
       </Routes>
-  </Router>
+    </Router>
   );
 }
 
 function Home() {
-  return <>
-          <h1>All Pets</h1>
-          <section>This is the page for all your pet needs</section>
-          <img src="imgs/kitten.jpg" alt="Kitten"/>
-          <img src="imgs/puppy.jpg" alt="Puppy"/>
-          <img src="imgs/duckling.jpg" alt="Duckling"/>
-        </>
+  return (
+    <>
+      <h1>All Pets</h1>
+      <section>This is the page for all your pet needs</section>
+      <img src="imgs/kitten.jpg" alt="Kitten" />
+      <img src="imgs/puppy.jpg" alt="Puppy" />
+      <img src="imgs/duckling.jpg" alt="Duckling" />
+    </>
+  );
 }
 
 function Kittens() {
-  return <>
+  return (
+    <>
       <h1>Kittens</h1>
       <section>This is the cats page</section>
-      <img src="imgs/kitten.jpg" alt="Kitten"/>
+      <img src="imgs/kitten.jpg" alt="Kitten" />
     </>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+function Puppies() {
+  return (
+    <>
+      <h1>Puppies</h1>
+      <section>This is the puppies page</section>
+      <img src="imgs/puppy.jpg" alt="Puppy" />
+    </>
+  );
+}
+function Ducklings() {
+  return (
+    <>
+      <h1>Ducklings</h1>
+      <section>This is the ducklings page</section>
+      <img src="imgs/duckling.jpg" alt="Duckling" />
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
