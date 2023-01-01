87af8036cedb8997266d0e0e8d2b4c521b7d8917
kxiru <nkeirukaw@gmail.com>
2023-01-01 23:36:22 +0000

    Completed Task 3

--- build-a-car/src/components/Car.js
+++ build-a-car/src/components/Car.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Wheel from "./Wheel";
 
 export default function Car(props) {
-  return <h2>Hi, I am a Car! I have {props.numberOfWheels} wheels</h2>;
+  let allWheels = [];
+  for (let i = 0; i < props.numberOfWheels; i++) {
+    allWheels.push(<Wheel />);
+  }
+
+  return (
+    <>
+      <h2>Hi, I am a Car! I have {props.numberOfWheels} wheels</h2>
+      {allWheels}
+    </>
+  );
 }
---
+++ build-a-car/src/components/Wheel.js
@@ -0,0 +1,9 @@
+export default function Wheel() {
+  return (
+    <img
+      src="build-a-car\src\imgs\wheel.png"
+      alt="Wheel"
+      style={{ width: "100px" }}
+    />
+  );
+}
