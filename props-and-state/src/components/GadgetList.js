18e1e7c52abaa6fe40bba9dd3dd7c4358a4a991c
kxiru <nkeirukaw@gmail.com>
2023-01-01 19:28:50 +0000

    Import external JSON data task completed

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const GadgetList = (props) => {
-
   return (
     <main>
       <h2>Headphones</h2>
       {props.items.map((item) => (
-      <div>
-        <div style={{fontWeight:'bold'}}>{item.title}</div><br></br>
-        <div>{item.price}</div>
-        <hr></hr>
-      </div>
+        <div key={item.key}>
+          <img src={item.src} alt="" />
+          <div style={{ fontWeight: "bold" }}>{item.title}</div>
+          <br></br>
+          <div>{item.price}</div>
+          <hr></hr>
+        </div>
       ))}
     </main>
   );
-}
+};
 
 export default GadgetList;
--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -1,20 +1,20 @@
 import React, { useState, Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import ClickCounter from "./components/ClickCounter";
-//import data from './models/headphones.json';
+import data from "./models/headphones.json";
 
 import GadgetList from "./components/GadgetList";
 const mainStyle = { fontFamily: "Arial" };
 
 const App = () => {
-  const headphones = [
-    { title: "Headphones 1", price: 1358.99 },
-    { title: "Headphones 2", price: 31.58 },
-    { title: "Headphones 3", price: 58.99 },
-    { title: "Headphones 4", price: 128.99 },
-    { title: "Headphones 5", price: 358.99 },
-  ];
-  const [headphonesList] = useState(headphones);
+  // const headphones = [
+  //   { title: "Headphones 1", price: 1358.99 },
+  //   { title: "Headphones 2", price: 31.58 },
+  //   { title: "Headphones 3", price: 58.99 },
+  //   { title: "Headphones 4", price: 128.99 },
+  //   { title: "Headphones 5", price: 358.99 },
+  // ];
+  const [headphonesList] = useState(data);
 
   return (
     <section style={mainStyle}>
