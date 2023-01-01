6f676494df914f5280602353f39e0e4686821a43
kxiru <nkeirukaw@gmail.com>
2023-01-01 22:36:02 +0000

    'Add About Page' homework completed

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import Book from "./components/Book";
+// import Book from "./components/Book";
 import data from "./models/books.json";
 import BookList from "./components/BookList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
 
 const App = (props) => {
   const [books] = useState(data);
@@ -10,6 +13,46 @@ const App = (props) => {
     console.log(`The Book '${title}' was clicked`);
   }
 
-  return <BookList books={books} addBook={addBook} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Header />
+              <h2>Welcome to the Bookcase App</h2>
+              <BookList books={books} addBook={addBook} />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/bookcase"
+          element={
+            <>
+              <Header />
+              <h2>Welcome to the Bookcase</h2>
+              <BookList books={books} addBook={addBook} />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
+
 export default App;
+
+// <BookList books={books} addBook={addBook} />;
