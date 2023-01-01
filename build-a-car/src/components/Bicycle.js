46ab799148f0aa97bf0d45f1ad570cf76c371c09
kxiru <nkeirukaw@gmail.com>
2023-01-01 23:13:20 +0000

    Write Other Components Task completed

---
+++ build-a-car/src/components/Bicycle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Bicycle() {
+  return <h2>good Morning, I am a Bicycle</h2>;
+}
---
+++ build-a-car/src/components/Skateboard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Skateboard() {
+  return <h2>Hey, I'm a Skateboard</h2>;
+}
